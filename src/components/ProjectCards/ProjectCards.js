29a1af1cd076e0ac3dfc3c5eed463828b6379754
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-03-12 15:29:13 -0800

    utilize react bootstrap

--- src/components/ProjectCards/ProjectCards.js
+++ src/components/ProjectCards/ProjectCards.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import ProjectCard from '../../containers/ProjectCard/ProjectCard';
 import beeImage from '../../assets/images/bee-your-bookkeeper.png';
@@ -7,21 +8,27 @@ import sqlImage from '../../assets/images/sql-search-engine.png';
 
 const ProjectCards = () => {
     return (
-        <Fragment>
-            <ProjectCard
-                image={beeImage}
-                title="Bee Your Bookkeeper"
-                description="A web-based family budget app utilizing React and AWS Lambda, API Gateway, DynamoDB."
-                details="To Be Add"
-                demo="https://master.da8crtrtwkv7x.amplifyapp.com/yourbee/"
-            />
-            <ProjectCard
-                image={sqlImage}
-                title="Structure Data Search Engine"
-                description="A search engine for structure data, utilizing reverse indexing and lazy data fetching."
-                details="To Be Add"
-                demo="https://yoshuanl.github.io/searchEngine.github.io/" />
-        </Fragment>
+        <Container fluid>
+            <Row>
+                <Col>
+                    <ProjectCard
+                        image={beeImage}
+                        title="Bee Your Bookkeeper"
+                        description="A web-based family budget app utilizing React and AWS Lambda, API Gateway, DynamoDB."
+                        details="To Be Add"
+                        demo="https://master.da8crtrtwkv7x.amplifyapp.com/yourbee/"
+                    />
+                </Col>
+                <Col>
+                    <ProjectCard
+                        image={sqlImage}
+                        title="Structure Data Search Engine"
+                        description="A search engine for structure data, utilizing reverse indexing and lazy data fetching."
+                        details="To Be Add"
+                        demo="https://yoshuanl.github.io/searchEngine.github.io/" />
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
