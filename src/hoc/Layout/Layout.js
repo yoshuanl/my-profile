aa5eb319203a431bb8caefee2a7bf1988fe8c905
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-03-12 09:47:10 -0800

    designed layout

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class Layout extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>- My Projects -</h1>
+                <div>project cards</div>
+                <div>footer: social icons, copyright</div>
+            </Fragment>
+        )
+    }
+};
+
+export default Layout;
