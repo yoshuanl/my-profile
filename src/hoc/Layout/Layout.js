f10b667ed18a16b0dec9171a7579fb0da8465cac
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-03-14 18:47:06 -0700

    adjust layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,15 +1,31 @@
 import React, { Component, Fragment } from 'react';
 
+import AboutMe from '../../components/AboutMe/AboutMe';
 import ProjectCards from '../../components/ProjectCards/ProjectCards';
 import SocialIcons from '../../containers/SocialIcons/SocialIcons';
+
+import './Layout.css';
 
 class Layout extends Component {
     render() {
         return (
             <Fragment>
-                <h1>- My Projects -</h1>
-                <ProjectCards />
-                <SocialIcons />
+                <div className="Block">
+                    <h1>- About Me -</h1>
+                    <br />
+                    <AboutMe />
+                </div>
+
+                <div className="Block">
+                    <h1>- My Projects -</h1>
+                    <br />
+                    <ProjectCards />
+                </div>
+
+                <div className="Block">
+                    <SocialIcons />
+                </div>
+
                 <p>©2021 YoShuanLiu</p>
             </Fragment>
         )
