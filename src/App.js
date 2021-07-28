8e23b2a8597200b1b357b51c545288f0dab6b95d
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-07-27 19:15:02 -0700

    use my icon

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 import Layout from './hoc/Layout/Layout';
 
+import scullyIcon from './assets/images/scullyicon.png';
+
 function App() {
   return (
     <div className="App">
-      <div className="Name">Scully Liu</div>
+      <div className="Name"><img className="MyIcon" src={scullyIcon} width="60" display="inline"/> Scully Liu</div>
       <Layout />
     </div>
   );
