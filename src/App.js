9bf6fa55873bd9d5a2c4c1aff896fa4be6812e3b
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-07-27 22:32:33 -0700

    add a navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
 import Layout from './hoc/Layout/Layout';
 
 import scullyIcon from './assets/images/scullyicon.png';
@@ -7,7 +9,20 @@ import scullyIcon from './assets/images/scullyicon.png';
 function App() {
   return (
     <div className="App">
-      <div className="Name"><img className="MyIcon" src={scullyIcon} width="60" display="inline"/> Scully Liu</div>
+      <Navbar bg="dark" variant="dark" sticky="top" >
+        <Container>
+          <Navbar.Brand href="#home" className="Icon">
+            <img
+              src={scullyIcon} width="50"
+            />{' '}
+            <strong>Scully Liu</strong>
+          </Navbar.Brand>
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#projects">My Projects</Nav.Link>
+          </Nav> */}
+        </Container>
+      </Navbar>
+      <br /><br /><br />
       <Layout />
     </div>
   );
